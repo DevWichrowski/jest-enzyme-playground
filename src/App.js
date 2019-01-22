b307305d615796f12def6a12024ab520eefb0ec6
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-22 20:48:25 +0100

    App renders without crash

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: 0
+    }
+  }
+
   render() {
+    const incrementValue = () =>{
+     this.setState({value: this.state.value + 1});
+    }
+
     return (
-      <div className="App">
-        <h1>Hello Test!</h1>
+      <div className="App" data-test='component-app'>
+        <h1>Current value: {this.state.value}</h1>
+        <button onClick={() => incrementValue()}>Click</button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
-import ReactDOM from 'react-dom';
 import App from "./App";
 
 Enzyme.configure({adapter: new EnzymeAdapter()});
 
-test('Apps render without crashes', () => {
+test('App renders without crash', () => {
     const wrapper = shallow(<App />); // wrappuje <App /> component, moze wrappowac zawsze o 1 poziom w doł, oczywiscie JSX
-    expect(wrapper).toBeTruthy();
+    const appComponent = wrapper.find("[data-test='component-app']"); // tak jakby selector z App
+    expect(appComponent.length).toBe(1); // oczekujemy ze bedzie 1 element
 });
 
-test('Fake test', () => {
-    expect(true).toBeTruthy();
+test('App renders incremnet button', () =>{
+
+});
+
+test('renders counter display', () =>{
+
+});
+
+test('counter start at 0', () =>{
+
+});
+
+test('clicking button increment a display', () =>{
+
 });
