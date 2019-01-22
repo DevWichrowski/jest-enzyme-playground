1480112ae55b559d5ff4aa4809f40fa9b62cb798
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-01-22 20:14:30 +0100

    Add configure and basics tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, {shallow} from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
+import ReactDOM from 'react-dom';
+import App from "./App";
 
 Enzyme.configure({adapter: new EnzymeAdapter()});
 
-test('renders without crashing', () => {
+test('Apps render without crashes', () => {
+    const wrapper = shallow(<App />); // wrappuje <App /> component, moze wrappowac zawsze o 1 poziom w doł, oczywiscie JSX
+    expect(wrapper).toBeTruthy();
+});
 
+test('Fake test', () => {
+    expect(true).toBeTruthy();
 });
